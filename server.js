467e482783372adaa78bf66cb19b415b030ebd32
Fidellis-Taz <fidellis.dango@gmail.com>
2021-03-02 16:29:46 +0300

    dabase connected

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
+mongoose.connect(prcess.env.DATABASE_URL, { useNewUrlParser });
 
-app.listen(3000, ()=>{
-    console.log("server has start");
-    
-})
+const db = mongoose.connection;
+
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("connected to database"));
+
+app.listen(3000, () => {
+  console.log("server has start");
+});
