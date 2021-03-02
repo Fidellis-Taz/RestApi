0ac6334f9e68c6bceb75255fb00ed5e7a387f929
Fidellis-Taz <fidellis.dango@gmail.com>
2021-03-02 20:52:06 +0300

    api set

---
+++ routes/subscribers.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const Subscriber = require("../models/subscriber");
+//get all subscribers
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+//get a specific or one subscriber
+router.get("/:id", getSubscriber, (req, res) => {
+  res.json(res.subscriber);
+});
+
+//create one
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+//update user:: we use patch instead of put becoz we only wanna update changes not everything
+router.patch("/:id", getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+//delete user
+router.delete("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ message: "Deleted Subscriber" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/* middleware for params*/
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: "Cannot find subscriber" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(prcess.env.DATABASE_URL, { useNewUrlParser });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
@@ -14,7 +14,7 @@ db.once("open", () => console.log("connected to database"));
 app.use(express.json());
 
 const subscribersRouter = require("./routes/subscribers");
-app.use("/subsribers", subscribersRouter);
+app.use("/subscribers", subscribersRouter);
 
 app.listen(3000, () => {
   console.log("server has start");
